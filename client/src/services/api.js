b52b9f8b6e9246118405a7a7746aae2c3dd1bcdc
Sumitchaurasiya10 <sumitchaurasiya1021@gmail.com>
2025-08-27 13:35:11 +0530

    env changes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL:'http://localhost:5000/api' });
+const API = axios.create({ 
+  // baseURL:'http://localhost:5000/api'
+  //baseURL:'https://travelmate-plus.onrender.com'
+  baseURL:process.env.REACT_APP_BASE_URL || "http://localhost:5000/api"
+ });
 
 API.interceptors.request.use((req) => {
   const user = JSON.parse(localStorage.getItem('user') || 'null');
